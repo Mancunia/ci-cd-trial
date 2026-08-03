730528c0802ebd3cce60c2cd76519fc799e6a640
Emmanuel Osei Mensah <mancuniamoe@gmail.com>
2026-08-03 16:53:48 +0000

    fix: pipeline fixes 😎

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // get the port from env variable
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5001;
 app.use(express.static("dist"));
 
 const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
+  await app.listen(PORT);
+  console.log(`server started on port ${PORT}`);
 }
 
 start()
